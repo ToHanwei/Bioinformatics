4765fd4c64c0bc6bb087bac592823eaad93c3f82
ToHanwei <hanwei@shanghaitech.edu.cn>
2021-05-28 10:20:39 +0800

    task4

---
+++ 2-Interactive_Text_for_week2/task4/ApproximatePatternMatching.py
@@ -0,0 +1,20 @@
+import sys
+
+infile = sys.argv[1]
+
+with open(infile) as ofile:
+	pat, text, dist = ofile.readlines()
+	pat = pat.strip()
+	text = text.strip()
+	dist = int(dist.strip())
+
+text_len = len(text)
+pat_len = len(pat)
+starts = []
+for i in range(text_len - pat_len + 1):
+	substr = text[i:i+pat_len]
+	count = sum(1 for x, y in zip(substr, pat) if x != y)
+	if count <= dist:
+		starts.append(i)
+
+print(*starts)	
